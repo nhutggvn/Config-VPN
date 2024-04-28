048d96076097891c32837492f5c2c0bcb1f69ecb
nhutggvn <nhut.nguyenminh.gg@gmail.com>
2024-04-28 09:13:26 +0700

    update file

---
+++ js/Auto_join_TF.js
@@ -0,0 +1,102 @@
+/*
+Script Author: DecoAri
+Fix Author: xream
+Reference: https://raw.githubusercontent.com/DecoAri/JavaScript/main/Surge/Auto_join_TF.js
+Thanks to a great person for adapting this script into Loon version!
+*/
+!(async () => {
+  let ids = $persistentStore.read("APP_ID");
+  if (ids == null) {
+    $notification.post(
+      "TestFlight APP_ID Not Added",
+      "Please manually add or use TestFlight link to auto-fetch.",
+      ""
+    );
+  } else if (ids == "") {
+    $notification.post(
+      "All TestFlight Apps Joined",
+      "Please disable this plugin manually.",
+      ""
+    );
+  } else {
+    ids = ids.split(",");
+    for await (const ID of ids) {
+      await autoPost(ID);
+    }
+  }
+  $done();
+})();
+
+function autoPost(ID) {
+  let Key = $persistentStore.read("key");
+  let testurl = "https://testflight.apple.com/v3/accounts/" + Key + "/ru/";
+  let header = {
+    "X-Session-Id": `${$persistentStore.read("session_id")}`,
+    "X-Session-Digest": `${$persistentStore.read("session_digest")}`,
+    "X-Request-Id": `${$persistentStore.read("request_id")}`,
+    "User-Agent": `${$persistentStore.read("tf_ua")}`,
+  };
+  return new Promise(function (resolve) {
+    $httpClient.get(
+      { url: testurl + ID, headers: header },
+      function (error, resp, data) {
+        if (error == null) {
+          if (resp.status == 404) {
+            let ids = $persistentStore.read("APP_ID").split(",");
+            ids = ids.filter((ids) => ids !== ID);
+            $persistentStore.write(ids.toString(), "APP_ID");
+            console.log(
+              ID +
+                " " +
+                "TestFlight does not exist, the APP_ID has been automatically removed."
+            );
+            $notification.post(
+              ID,
+              "TestFlight Not Found",
+              "The APP_ID has been automatically removed."
+            );
+            resolve();
+          } else {
+            let jsonData = JSON.parse(data);
+            if (jsonData.data == null) {
+              console.log(ID + " " + jsonData.messages[0].message);
+              resolve();
+            } else if (jsonData.data.status == "FULL") {
+              console.log(
+                jsonData.data.app.name + " " + ID + " " + jsonData.data.message
+              );
+              resolve();
+            } else {
+              $httpClient.post(
+                { url: testurl + ID + "/accept", headers: header },
+                function (error, resp, body) {
+                  let jsonBody = JSON.parse(body);
+                  $notification.post(
+                    jsonBody.data.name,
+                    "TestFlight Joined Successfully",
+                    ""
+                  );
+                  console.log(
+                    jsonBody.data.name + " TestFlight Joined Successfully"
+                  );
+                  let ids = $persistentStore.read("APP_ID").split(",");
+                  ids = ids.filter((ids) => ids !== ID);
+                  $persistentStore.write(ids.toString(), "APP_ID");
+                  resolve();
+                }
+              );
+            }
+          }
+        } else {
+          if (error == "The request timed out.") {
+            resolve();
+          } else {
+            $notification.post("Auto Join TestFlight", error, "");
+            console.log(ID + " " + error);
+            resolve();
+          }
+        }
+      }
+    );
+  });
+}
---
+++ js/TF_keys.js
@@ -0,0 +1,59 @@
+/*
+Script Author: DecoAri
+Reference: https://github.com/DecoAri/JavaScript/blob/main/Surge/TF_keys.js
+Specific Usage Steps:
+1: Import the plugin.
+2: Enable Mitm over Http2 on the Mitm page.
+3: Start VPN, enter TestFlight App, and display notification of successful information retrieval.
+4: Go to Configuration -> Persistent Data -> Import Specified Data. Fill in key with "APP_ID", and value with the ID of the TF you want to join. (ID is the string after "join" in the link https://testflight.apple.com/join/LPQmtkUs, for example, "LPQmtkUs".) ⚠️: Supports an unlimited number of TF links, each link needs to be separated by a comma (such as: LPQmtkUs, Hgun65jg, 8yhJgv)
+*/
+
+const reg1 = /^https:\/\/testflight\.apple\.com\/v3\/accounts\/(.*)\/apps$/;
+const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/;
+
+if (reg1.test($request.url)) {
+    $persistentStore.write(null, 'request_id');
+    let url = $request.url;
+    let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2');
+    let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id'];
+    let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest'];
+    let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id'];
+    let ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
+
+    $persistentStore.write(key, 'key');
+    $persistentStore.write(session_id, 'session_id');
+    $persistentStore.write(session_digest, 'session_digest');
+    $persistentStore.write(request_id, 'request_id');
+    $persistentStore.write(ua, 'tf_ua');
+
+    console.log($request.headers);
+
+    if ($persistentStore.read('request_id') !== null) {
+        $notification.post('TF Information Acquisition', 'Information acquisition successful. Please close the script!', '');
+    } else {
+        $notification.post('TF Information Acquisition', 'Information acquisition failed. Please turn on Mitm over HTTP2, and restart VPN and TestFlight App!', '');
+    }
+
+    $done({});
+}
+
+if (reg2.test($request.url)) {
+    let appId = $persistentStore.read("APP_ID");
+    if (!appId) {
+        appId = "";
+    }
+    let arr = appId.split(",");
+    const id = reg2.exec($request.url)[1];
+    arr.push(id);
+    arr = unique(arr).filter((a) => a);
+    if (arr.length > 0) {
+        appId = arr.join(",");
+    }
+    $persistentStore.write(appId, "APP_ID");
+    $notification.post("TestFlight Auto Join", `APP_ID added: ${id}`, `Current IDs: ${appId}`);
+    $done({});
+}
+
+function unique(arr) {
+    return Array.from(new Set(arr));
+}
